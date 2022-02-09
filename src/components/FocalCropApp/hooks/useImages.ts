b4813b5149daf6e93e5bf78a7072d6890e981220
Tyler Rowsell <tyler.rowsell@shopify.com>
2022-02-09 16:25:49 -0500

    fix unused

--- src/components/FocalCropApp/hooks/useImages.ts
+++ src/components/FocalCropApp/hooks/useImages.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as generateUuid } from 'uuid';
 import {Coordinate, Region, StoredImage, ImagesObject} from '../types';
 import { getImageDimensions } from '../utilities';
